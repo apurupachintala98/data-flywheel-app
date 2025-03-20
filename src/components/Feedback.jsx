cca79762ee596de4e784b5a496451c0ad926a486
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-03-20 13:33:39 +0530

    push

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -3,6 +3,25 @@ import { Box, IconButton, Typography, Tooltip } from '@mui/material';
 
 const Feedback = ({ message }) => {
     const [feedback, setFeedback] = useState(null);
+    const [isSpeaking, setIsSpeaking] = useState(false);
+    const synthRef = useRef(window.speechSynthesis);
+
+    // const handleSpeak = () => {
+    //     if (!message || !message.text) {
+    //         console.error("Message is undefined or empty");
+    //         return;
+    //     }
+
+    //     if ('speechSynthesis' in window) {
+    //         const synth = window.speechSynthesis;
+    //         const utterance = new SpeechSynthesisUtterance(message.text);
+
+    //         synth.cancel(); // Stop any ongoing speech
+    //         synth.speak(utterance);
+    //     } else {
+    //         alert("Speech synthesis is not supported in this browser.");
+    //     }
+    // };
 
     const handleSpeak = () => {
         if (!message || !message.text) {
@@ -10,16 +29,29 @@ const Feedback = ({ message }) => {
             return;
         }
 
-        if ('speechSynthesis' in window) {
-            const synth = window.speechSynthesis;
-            const utterance = new SpeechSynthesisUtterance(message.text);
+        if ("speechSynthesis" in window) {
+            const synth = synthRef.current;
 
-            synth.cancel(); // Stop any ongoing speech
+            // Stop any ongoing speech
+            if (synth.speaking) {
+                synth.cancel();
+                setIsSpeaking(false);
+                return;
+            }
+
+            const utterance = new SpeechSynthesisUtterance(message.text);
+            utterance.rate = 1.0;  // Normal speed
+            utterance.pitch = 1.0; // Normal pitch
+            utterance.volume = 1.0; // Max volume
+
+            utterance.onend = () => setIsSpeaking(false);
             synth.speak(utterance);
+            setIsSpeaking(true);
         } else {
             alert("Speech synthesis is not supported in this browser.");
         }
     };
+
 
     const handleCopy = async () => {
         if (!message || !message.text) {
@@ -51,11 +83,26 @@ const Feedback = ({ message }) => {
                 <IconButton>
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon-md-heavy"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.8727 21.4961C11.6725 21.8466 11.2811 22.0423 10.8805 21.9922L10.4267 21.9355C7.95958 21.6271 6.36855 19.1665 7.09975 16.7901L7.65054 15H6.93226C4.29476 15 2.37923 12.4921 3.0732 9.94753L4.43684 4.94753C4.91145 3.20728 6.49209 2 8.29589 2H18.0045C19.6614 2 21.0045 3.34315 21.0045 5V12C21.0045 13.6569 19.6614 15 18.0045 15H16.0045C15.745 15 15.5054 15.1391 15.3766 15.3644L11.8727 21.4961ZM14.0045 4H8.29589C7.39399 4 6.60367 4.60364 6.36637 5.47376L5.00273 10.4738C4.65574 11.746 5.61351 13 6.93226 13H9.00451C9.32185 13 9.62036 13.1506 9.8089 13.4059C9.99743 13.6612 10.0536 13.9908 9.96028 14.2941L9.01131 17.3782C8.6661 18.5002 9.35608 19.6596 10.4726 19.9153L13.6401 14.3721C13.9523 13.8258 14.4376 13.4141 15.0045 13.1902V5C15.0045 4.44772 14.5568 4 14.0045 4ZM17.0045 13V5C17.0045 4.64937 16.9444 4.31278 16.8338 4H18.0045C18.5568 4 19.0045 4.44772 19.0045 5V12C19.0045 12.5523 18.5568 13 18.0045 13H17.0045Z" fill="currentColor"></path></svg>
                 </IconButton></Tooltip>
-            <Tooltip title="Read Aloud">
+            {/* <Tooltip title="Read Aloud">
 
                 <IconButton onClick={handleSpeak}>
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon-md-heavy"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 4.9099C11 4.47485 10.4828 4.24734 10.1621 4.54132L6.67572 7.7372C6.49129 7.90626 6.25019 8.00005 6 8.00005H4C3.44772 8.00005 3 8.44776 3 9.00005V15C3 15.5523 3.44772 16 4 16H6C6.25019 16 6.49129 16.0938 6.67572 16.2629L10.1621 19.4588C10.4828 19.7527 11 19.5252 11 19.0902V4.9099ZM8.81069 3.06701C10.4142 1.59714 13 2.73463 13 4.9099V19.0902C13 21.2655 10.4142 22.403 8.81069 20.9331L5.61102 18H4C2.34315 18 1 16.6569 1 15V9.00005C1 7.34319 2.34315 6.00005 4 6.00005H5.61102L8.81069 3.06701ZM20.3166 6.35665C20.8019 6.09313 21.409 6.27296 21.6725 6.75833C22.5191 8.3176 22.9996 10.1042 22.9996 12.0001C22.9996 13.8507 22.5418 15.5974 21.7323 17.1302C21.4744 17.6185 20.8695 17.8054 20.3811 17.5475C19.8927 17.2896 19.7059 16.6846 19.9638 16.1962C20.6249 14.9444 20.9996 13.5175 20.9996 12.0001C20.9996 10.4458 20.6064 8.98627 19.9149 7.71262C19.6514 7.22726 19.8312 6.62017 20.3166 6.35665ZM15.7994 7.90049C16.241 7.5688 16.8679 7.65789 17.1995 8.09947C18.0156 9.18593 18.4996 10.5379 18.4996 12.0001C18.4996 13.3127 18.1094 14.5372 17.4385 15.5604C17.1357 16.0222 16.5158 16.1511 16.0539 15.8483C15.5921 15.5455 15.4632 14.9255 15.766 14.4637C16.2298 13.7564 16.4996 12.9113 16.4996 12.0001C16.4996 10.9859 16.1653 10.0526 15.6004 9.30063C15.2687 8.85905 15.3578 8.23218 15.7994 7.90049Z" fill="currentColor"></path></svg>
-                </IconButton> </Tooltip>
+                </IconButton> </Tooltip> */}
+                 <Tooltip title={isSpeaking ? "Stop Reading" : "Read Aloud"}>
+                <IconButton onClick={handleSpeak}>
+                    {isSpeaking ? (
+                        // Stop button SVG
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" clipRule="evenodd" d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM9.5 8.5C8.94772 8.5 8.5 8.94772 8.5 9.5V14.5C8.5 15.0523 8.94772 15.5 9.5 15.5H14.5C15.0523 15.5 15.5 15.0523 15.5 14.5V9.5C15.5 8.94772 15.0523 8.5 14.5 8.5H9.5Z" fill="currentColor"></path>
+                        </svg>
+                    ) : (
+                        // Volume button SVG
+                        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" clipRule="evenodd" d="M11 4.9099C11 4.47485 10.4828 4.24734 10.1621 4.54132L6.67572 7.7372C6.49129 7.90626 6.25019 8.00005 6 8.00005H4C3.44772 8.00005 3 8.44776 3 9.00005V15C3 15.5523 3.44772 16 4 16H6C6.25019 16 6.49129 16.0938 6.67572 16.2629L10.1621 19.4588C10.4828 19.7527 11 19.5252 11 19.0902V4.9099ZM8.81069 3.06701C10.4142 1.59714 13 2.73463 13 4.9099V19.0902C13 21.2655 10.4142 22.403 8.81069 20.9331L5.61102 18H4C2.34315 18 1 16.6569 1 15V9.00005C1 7.34319 2.34315 6.00005 4 6.00005H5.61102L8.81069 3.06701Z" fill="currentColor"></path>
+                        </svg>
+                    )}
+                </IconButton>
+            </Tooltip>
         </div>
     );
 };
